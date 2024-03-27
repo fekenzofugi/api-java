2b6b47625f2348a21ae82a81de1ce7586138aaa3
fekenzofugi <fernandofugihara@gmail.com>
2024-03-26 23:35:13 -0300

    first commit

---
+++ src/main/java/com/example/crud/user/User.java
@@ -0,0 +1,43 @@
+package com.example.crud.user;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
